2e3bf249833a81ebc5928eb9cec5017bbc57f879
Jacob Coffee <jacob@z7x.org>
2023-11-06 00:53:46 -0600

    feat(beta): add embed help command

--- src/byte/commands/testing.py
+++ src/byte/commands/testing.py
@@ -1,6 +1,7 @@
 """Commands for testing purposes."""
 from __future__ import annotations
 
+from discord import Embed
 from discord.ext import commands
 
 
@@ -32,9 +33,23 @@ class TestingCommands(commands.Cog):
         Args:
             ctx: Context object.
         """
-        print(f"pinged by {ctx.author}")
+        print(f"pinged by {ctx.author}")  # noqa: T201
         guild = ctx.guild.id
         await ctx.send(f"pong to {guild}")
+
+    @commands.command(name="gethelp")
+    async def helpme(self, ctx: commands.Context) -> None:
+        """Provides help resources in a card-style embed with a thumbnail."""
+        issues = "https://github.com/litestar-org/litestar/issues"
+        embed = Embed(title="Need Help?", color=0x42B1A8)  # Updated color to #42b1a8
+        embed.add_field(name="GitHub Issues", value=f"[Click here to report an issue]({issues})")
+        embed.add_field(name="Help Channel", value="<#1064114019373432912>")
+        embed.set_footer(text="Please provide a Minimal, Complete, and Verifiable example (MCVE) if applicable.")
+        embed.set_thumbnail(
+            url="https://github.com/litestar-org/branding/blob/main/assets/Branding%20-%20PNG%20-%20Transparent/Badge%20-%20White.png?raw=true"
+        )
+
+        await ctx.send(embed=embed)
 
 
 async def setup(bot: commands.Bot) -> None:
