9aa3af5fffd231f61fbd25ae90e2ce8336e3b6a5
Jacob Coffee <jacob@z7x.org>
2023-12-10 13:51:08 -0600

    feat: add custom plugins for ``litestar`` guild

--- src/byte/plugins/__init__.py
+++ src/byte/plugins/__init__.py
@@ -1,8 +1,9 @@
 """Pluggable modules for Byte."""
 
-from src.byte.plugins import admin, events, forums, general, testing
+from src.byte.plugins import admin, custom, events, forums, general, testing
 
 __all__ = [
+    "custom",
     "admin",
     "events",
     "forums",
---
+++ src/byte/plugins/custom/litestar.py
@@ -0,0 +1,94 @@
+"""Custom plugins for the Litestar Discord."""
+from __future__ import annotations
+
+from discord import Embed
+from discord.ext.commands import Bot, Cog, Context, command, group, is_owner
+
+from src.byte.lib.utils import is_byte_dev, mention_role, mention_user
+
+__all__ = ("LitestarCommands", "setup")
+
+
+class LitestarCommands(Cog):
+    """Litestar command cog."""
+
+    def __init__(self, bot: Bot) -> None:
+        """Initialize cog."""
+        self.bot = bot
+        self.__cog_name__ = "Litestar Commands"  # type: ignore[misc]
+
+    @group(name="litestar")
+    @is_byte_dev()
+    async def litestar(self, ctx: Context) -> None:
+        """Commands for the Litestar guild."""
+        if ctx.invoked_subcommand is None:
+            await ctx.send("Invalid Litestar command passed...")
+            await ctx.send_help(ctx.command)
+
+    @command(
+        name="apply-role-embed",
+        help="Apply the role information embed to a channel.",
+        aliases=["are"],
+        hidden=True,
+    )
+    @is_owner()
+    async def apply_role_embed(self, ctx: Context) -> None:
+        """Apply the role information embed to a channel.
+
+        Args:
+            ctx: Context object.
+        """
+        embed = Embed(title="Litestar Roles", color=0x42B1A8)
+
+        embed.add_field(name="Organization Roles", value="\u200b", inline=False)
+        embed.add_field(
+            name=f"{mention_role(919261960921546815)}", value="Maintainers of the Litestar organization", inline=False
+        )
+        embed.add_field(
+            name=f"{mention_role(1084813023044173866)}",
+            value="Members invited to the Litestar organization due to their contributions",
+            inline=False,
+        )
+        embed.add_field(
+            name=f"{mention_role(1059206375814737930)}",
+            value="Users that have created 3rd party libraries for Litestar projects",
+            inline=False,
+        )
+        embed.add_field(
+            name=f"{mention_role(1150487028400652339)}",
+            value="Users that were once maintainers of a Litestar project or the Litestar organization",
+            inline=False,
+        )
+        embed.add_field(
+            name=f"{mention_role(919261960921546815)}",
+            value=f"Programs providing services within the community. " f"(like {mention_user(1132179951567786015)}!)",
+            inline=False,
+        )
+
+        embed.add_field(name="Community Roles", value="\u200b", inline=False)
+        embed.add_field(
+            name=f"{mention_role(1102727999285121074)}",
+            value="Users that contribute financially through OpenCollective, Polar.sh, or GitHub Sponsors",
+            inline=False,
+        )
+        embed.add_field(
+            name=f"{mention_role(1150686603740729375)}",
+            value="Users that help moderate the Litestar community",
+            inline=False,
+        )
+        embed.add_field(
+            name=f"{mention_role(1152668020825661570)}",
+            value="Users that consistently help the members of the Litestar community",
+            inline=False,
+        )
+
+        await ctx.send(embed=embed)
+
+
+async def setup(bot: Bot) -> None:
+    """Add cog to bot.
+
+    Args:
+        bot: Bot object.
+    """
+    await bot.add_cog(LitestarCommands(bot))
