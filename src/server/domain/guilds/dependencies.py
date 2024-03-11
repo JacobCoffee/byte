a73c315c49cbdb2131c042fc54a2df75bea03b60
Jacob Coffee <jacob@z7x.org>
2024-03-10 23:23:59 -0500

    feat: add routes to display sections on guild data

--- src/server/domain/guilds/dependencies.py
+++ src/server/domain/guilds/dependencies.py
@@ -8,7 +8,13 @@ from sqlalchemy import select
 from sqlalchemy.orm import joinedload, noload, selectinload
 
 from server.domain.db.models import AllowedUsersConfig, ForumConfig, GitHubConfig, Guild, SOTagsConfig
-from server.domain.guilds.services import GuildsService
+from server.domain.guilds.services import (
+    AllowedUsersConfigService,
+    ForumConfigService,
+    GitHubConfigService,
+    GuildsService,
+    SOTagsConfigService,
+)
 from server.lib import log
 
 if TYPE_CHECKING:
@@ -16,7 +22,13 @@ if TYPE_CHECKING:
 
     from sqlalchemy.ext.asyncio import AsyncSession
 
-__all__ = ("provides_guilds_service",)
+__all__ = (
+    "provides_allowed_users_config_service",
+    "provides_forum_config_service",
+    "provides_github_config_service",
+    "provides_guilds_service",
+    "provides_sotags_config_service",
+)
 
 logger = log.get_logger()
 
@@ -47,6 +59,100 @@ async def provides_guilds_service(db_session: AsyncSession) -> AsyncGenerator[Gu
             selectinload(Guild.forum_config).options(
                 joinedload(ForumConfig.guild, innerjoin=True).options(noload("*")),
             ),
+        ),
+    ) as service:
+        try:
+            yield service
+        finally:
+            ...
+
+
+async def provides_github_config_service(db_session: AsyncSession) -> AsyncGenerator[GitHubConfigService, None]:
+    """Construct GitHubConfig-based repository and service objects for the request.
+
+    Args:
+        db_session (AsyncSession): SQLAlchemy AsyncSession
+
+    Yields:
+        GitHubConfigService: GitHubConfig-based service
+    """
+    async with GitHubConfigService.new(
+        session=db_session,
+        statement=select(GitHubConfig)
+        .order_by(GitHubConfig.github_organization)
+        .options(
+            selectinload(GitHubConfig.guild).options(noload("*")),
+        ),
+    ) as service:
+        try:
+            yield service
+        finally:
+            ...
+
+
+async def provides_sotags_config_service(db_session: AsyncSession) -> AsyncGenerator[SOTagsConfigService, None]:
+    """Construct SOTagsConfig-based repository and service objects for the request.
+
+    Args:
+        db_session (AsyncSession): SQLAlchemy AsyncSession
+
+    Yields:
+        SOTagsConfigService: SOTagsConfig-based service
+    """
+    async with SOTagsConfigService.new(
+        session=db_session,
+        statement=select(SOTagsConfig)
+        .order_by(SOTagsConfig.tag_name)
+        .options(
+            selectinload(SOTagsConfig.guild).options(noload("*")),
+        ),
+    ) as service:
+        try:
+            yield service
+        finally:
+            ...
+
+
+async def provides_allowed_users_config_service(
+    db_session: AsyncSession,
+) -> AsyncGenerator[AllowedUsersConfigService, None]:
+    """Construct AllowedUsersConfig-based repository and service objects for the request.
+
+    Args:
+        db_session (AsyncSession): SQLAlchemy AsyncSession
+
+    Yields:
+        AllowedUsersConfigService: AllowedUsersConfig-based service
+    """
+    async with AllowedUsersConfigService.new(
+        session=db_session,
+        statement=select(AllowedUsersConfig)
+        .order_by(AllowedUsersConfig.user_id)
+        .options(
+            selectinload(AllowedUsersConfig.guild).options(noload("*")),
+        ),
+    ) as service:
+        try:
+            yield service
+        finally:
+            ...
+
+
+async def provides_forum_config_service(db_session: AsyncSession) -> AsyncGenerator[ForumConfigService, None]:
+    """Construct ForumConfig-based repository and service objects for the request.
+
+    Args:
+        db_session (AsyncSession): SQLAlchemy AsyncSession
+
+    Yields:
+        ForumConfigService: ForumConfig-based service
+    """
+    async with ForumConfigService.new(
+        session=db_session,
+        statement=select(ForumConfig)
+        .order_by(ForumConfig.help_forum)
+        .options(
+            selectinload(ForumConfig.guild).options(noload("*")),
         ),
     ) as service:
         try:
